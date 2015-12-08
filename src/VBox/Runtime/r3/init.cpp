56eac5ced5cdcb15a0fc5398d18cca8d90ec450e
Dmitriy Kuminov <coding@dmik.org>
2015-12-08 00:30:49 +0000

    Runtime: Work around _FORK_CHILD1 usage in optimized builds.
    The GCC optimizer 4 seems to remove the variable declared with
    volatile if it's not used in normal code. Use extern instead.
    
    
    git-svn-id: http://svn.netlabs.org/repos/vbox/trunk@24 8b531da7-8337-e511-8bd3-62de69f6b5d0

--- src/VBox/Runtime/r3/init.cpp
+++ src/VBox/Runtime/r3/init.cpp
@@ -203,8 +203,8 @@ int rtR3ForkOs2Child(__LIBC_PFORKHANDLE pForkHandle, __LIBC_FORKOP enmOperation)
     return 0;
 }
 
-# define static static volatile /** @todo _FORK_CHILD1 causes unresolved externals in optimized builds. Fix macro. */
-_FORK_CHILD1(0, rtR3ForkOs2Child);
+# define static extern /** @todo _FORK_CHILD1 causes unresolved externals in optimized builds. Fix macro. */
+_FORK_CHILD1(0, rtR3ForkOs2Child)
 # undef static
 #endif /* RT_OS_OS2 */
 
