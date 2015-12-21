e18350278ed980aad0b5c50084a0c5550941be8a
Dmitriy Kuminov <coding@dmik.org>
2015-12-21 22:04:29 +0000

    Main: Enable VBOX_WITH_HOSTNETIF_API on OS/2 and add missing stubs.
    Author: @valerius.
    
    
    git-svn-id: http://svn.netlabs.org/repos/vbox/trunk@40 8b531da7-8337-e511-8bd3-62de69f6b5d0

--- src/VBox/Main/src-server/os2/NetIf-os2.cpp
+++ src/VBox/Main/src-server/os2/NetIf-os2.cpp
@@ -33,12 +33,13 @@ int NetIfList(std::list <ComObjPtr<HostNetworkInterface> > &list)
     return VERR_NOT_IMPLEMENTED;
 }
 
-int NetIfEnableStaticIpConfig(VirtualBox *pVBox, HostNetworkInterface * pIf, ULONG ip, ULONG mask)
+int NetIfEnableStaticIpConfig(VirtualBox *pVBox, HostNetworkInterface * pIf, ULONG aOldIp, ULONG aNewIp, ULONG aMask)
 {
     return VERR_NOT_IMPLEMENTED;
 }
 
-int NetIfEnableStaticIpConfigV6(VirtualBox *pVBox, HostNetworkInterface * pIf, IN_BSTR aIPV6Address, ULONG aIPV6MaskPrefixLength)
+int NetIfEnableStaticIpConfigV6(VirtualBox *pVBox, HostNetworkInterface * pIf, IN_BSTR aOldIPV6Address,
+                                IN_BSTR aIPV6Address, ULONG aIPV6MaskPrefixLength)
 {
     return VERR_NOT_IMPLEMENTED;
 }
@@ -48,9 +49,36 @@ int NetIfEnableDynamicIpConfig(VirtualBox *pVBox, HostNetworkInterface * pIf)
     return VERR_NOT_IMPLEMENTED;
 }
 
-
 int NetIfDhcpRediscover(VirtualBox *pVBox, HostNetworkInterface * pIf)
 {
     return VERR_NOT_IMPLEMENTED;
 }
 
+int NetIfGetState(const char *pcszIfName, NETIFSTATUS *penmState)
+{
+    return VERR_NOT_IMPLEMENTED;
+}
+
+int NetIfGetLinkSpeed(const char *pcszIfName, uint32_t *puMbits)
+{
+    return VERR_NOT_IMPLEMENTED;
+}
+
+int NetIfCreateHostOnlyNetworkInterface(VirtualBox *pVirtualBox,
+                                        IHostNetworkInterface **aHostNetworkInterface,
+                                        IProgress **aProgress,
+                                        const char *pcszName)
+{
+    return VERR_NOT_IMPLEMENTED;
+}
+
+int NetIfGetConfig(HostNetworkInterface * pIf, NETIFINFO *pInfo)
+{
+    return VERR_NOT_IMPLEMENTED;
+}
+
+int NetIfRemoveHostOnlyNetworkInterface(VirtualBox *pVirtualBox, IN_GUID aId,
+                                        IProgress **aProgress)
+{
+    return VERR_NOT_IMPLEMENTED;
+}
