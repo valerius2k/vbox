543afd018d6552eb75c8dbb71bb2f53f9f2cc180
Dmitriy Kuminov <coding@dmik.org>
2015-12-25 01:24:45 +0000

    Runtime: RTMpOnPair: Fix definition mismatch in generic impl.
    git-svn-id: http://svn.netlabs.org/repos/vbox/trunk@52 8b531da7-8337-e511-8bd3-62de69f6b5d0

--- src/VBox/Runtime/r0drv/generic/RTMpOn-r0drv-generic.cpp
+++ src/VBox/Runtime/r0drv/generic/RTMpOn-r0drv-generic.cpp
@@ -72,7 +72,7 @@ RTDECL(int) RTMpOnSpecific(RTCPUID idCpu, PFNRTMPWORKER pfnWorker, void *pvUser1
 RT_EXPORT_SYMBOL(RTMpOnSpecific);
 
 
-RTDECL(int) RTMpOnPair(RTCPUID idCpu1, RTCPUID idCpu2, PFNRTMPWORKER pfnWorker, void *pvUser1, void *pvUser2)
+RTDECL(int) RTMpOnPair(RTCPUID idCpu1, RTCPUID idCpu2, uint32_t fFlags, PFNRTMPWORKER pfnWorker, void *pvUser1, void *pvUser2)
 {
     NOREF(idCpu1);
     NOREF(idCpu2);
