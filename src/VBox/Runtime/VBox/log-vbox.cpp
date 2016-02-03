9807f7128f1a8de381d379604afc398eccd942af
Valery V. Sedletski <_valerius@mail.ru>
2016-02-03 05:52:32 +0000

    Runtime: Logging: Return back unneeded change.
    Undo unneeded change: uncommented vboxdrv logging.
    
    
    git-svn-id: http://svn.netlabs.org/repos/vbox/trunk@71 8b531da7-8337-e511-8bd3-62de69f6b5d0

--- src/VBox/Runtime/VBox/log-vbox.cpp
+++ src/VBox/Runtime/VBox/log-vbox.cpp
@@ -514,7 +514,7 @@ RTDECL(PRTLOGGER) RTLogDefaultInit(void)
         RTLogFlags(pLogger, "enabled unbuffered");
         pLogger->fDestFlags |= RTLOGDEST_DEBUGGER | RTLOGDEST_STDOUT;
 # endif
-# if 1 /* vboxdrv logging - ATTENTION: this is what we're referring to guys! Change to '# if 1'. */
+# if 0 /* vboxdrv logging - ATTENTION: this is what we're referring to guys! Change to '# if 1'. */
         RTLogGroupSettings(pLogger, "all=~0 -default.l6.l5.l4.l3");
         RTLogFlags(pLogger, "enabled unbuffered tid");
         pLogger->fDestFlags |= RTLOGDEST_DEBUGGER | RTLOGDEST_STDOUT;
